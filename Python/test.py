3dce7d1ed6c4ba137b161236d138eaf923c14ef8
dilukshashamal <dilukshashamal2001@gmail.com>
2024-10-04 08:13:47 +0530

    add python

---
+++ Python/test.py
@@ -0,0 +1,10 @@
+# need to create for loop to iterate through the list of numbers
+
+# list of numbers
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for loop to iterate through the list of numbers
+
+for number in numbers:
+    print(number)
